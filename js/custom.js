7696905cedbe1d1df519ec4d5c00852a41f64b03
javier <javipons@mail.com>
2019-09-16 17:32:14 -0300

    se agrego funcion expand pero no esta activa

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,21 @@
         // console.log(cantidadSetImagenes);
 })(jQuery);
 
-function expand() {
-    document.querySelector('.info').classList.add("expand");
+
+// Para hacer andar la funcion agregar onclick="expand('info')" en el link del menu
+function expand(element) {
+    var viewportHeight = $(window).height();
+    var elementHeight = $(".info").outerHeight();
+    var diffHeight = viewportHeight - elementHeight;
+    //var paddingPrevious = $(".info").css('padding-top');
+    var paddingToShow = diffHeight / 2;
+    var selectedElement = document.querySelector('.' + element);
+    var i = 0;
+    $(selectedElement).animate({ 'padding-top': paddingToShow + 'px', 'padding-bottom': paddingToShow + 'px' }, 600);
+    $(document).scroll(function() {
+        i++;
+        if (i > 1) {
+            $(selectedElement).animate({ 'padding-top': 0 + 'px', 'padding-bottom': 0 + 'px' }, 600);
+        }
+    });
 }
