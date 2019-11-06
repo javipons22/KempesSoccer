e51bbdf6f162f1a36251cb160551c7cc0fc4894a
Javier <javipons@mail.com>
2019-11-06 20:46:12 -0300

    google analytics

---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+function ValidarDatos($campo){
+//Array con las posibles cabeceras a utilizar por un spammer
+$badHeads = array("Content-Type:",
+"MIME-Version:",
+"Content-Transfer-Encoding:",
+"Return-path:",
+"Subject:",
+"From:",
+"Envelope-to:",
+"To:",
+"bcc:",
+"cc:");
+//Comprobamos que entre los datos no se encuentre alguna de
+//las cadenas del array. Si se encuentra alguna cadena se
+//dirige a una página de Forbidden
+foreach($badHeads as $valor){
+if(strpos(strtolower($campo), strtolower($valor)) !== false){
+header( "HTTP/1.0 403 Forbidden");
+exit;
+}
+}
+}
+//Ejemplo de llamadas a la funcion
+ValidarDatos($_POST['email']);
+ValidarDatos($_POST['asunto']);
+ValidarDatos($_POST['mensaje']);
+?>
