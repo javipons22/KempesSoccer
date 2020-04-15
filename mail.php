cf661e52b53acccd4f74d5b85056b367cb825d9a
Javier <javipons@mail.com>
2020-04-15 15:16:39 -0300

    php mailer agregado

--- mail.php
+++ mail.php
@@ -1,99 +1,58 @@
+
 <?php
-function ValidarDatos($campo){
-//Array con las posibles cabeceras a utilizar por un spammer
-$badHeads = array("Content-Type:",
-"MIME-Version:",
-"Content-Transfer-Encoding:",
-"Return-path:",
-"Subject:",
-"From:",
-"Envelope-to:",
-"To:",
-"bcc:",
-"cc:");
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\Exception;
+// use PHPMailer\PHPMailer\SMTP;
 
-$value = array();
-//Comprobamos que entre los datos no se encuentre alguna de
-//las cadenas del array. Si     se encuentra alguna cadena se
-//dirige a una página de Forbidden
-    foreach($badHeads as $valor){
-        if(strpos(strtolower($campo), strtolower($valor)) !== false){
-            $value[] = 1;
-            
-        } else {
-            $value[] = 0;
-        }
-    }
-if (array_search(1,$value) === false) {
-    return true;
-} else {
-    return false;
-}
-}
+//require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+//require 'PHPMailer/src/SMTP.php';
 
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$mail->SMTPDebug = 0;
 // //Ejemplo de llamadas a la funcion
 $nombre = $_POST['firstname'];
 $pais = $_POST['pais'];
 $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
 
-$errorMSG = "";
 
-// TELEFONO
-if (empty($nombre) || empty($pais) || empty($email) || empty($mensaje)) {
-    $errorMSG = "1";
-}
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    // $mail->isSMTP();                                            // Send using SMTP
+    // $mail->Host       = 'dtcwin155.ferozo.com';                    // Set the SMTP server to send through
+    // $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    // $mail->Username   = 'no-reply@w1551599.ferozo.com';                     // SMTP username
+    // $mail->Password   = 'wivifu83VE';                               // SMTP password
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    // $mail->Port       = 25;                                 // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-$nombre1 = ValidarDatos($nombre);
-$pais1 = ValidarDatos($pais);
-$email1 = ValidarDatos($email);
-$mensaje1 = ValidarDatos($mensaje);
+    //Recipients
+    $mail->setFrom('hola@kempessoccer.info', 'Formulario KEMPES SOCCER');
+    $mail->addAddress('hola@kempessoccer.info', 'Kempes');     // Add a recipient
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
 
-$nombre2 = filter_var( $nombre, FILTER_SANITIZE_STRING);
-$email2 = filter_var( $email, FILTER_VALIDATE_EMAIL);
-$pais2 = filter_var( $pais, FILTER_SANITIZE_STRING);
-$mensaje2 = filter_var( $mensaje, FILTER_SANITIZE_STRING);
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Nuevo contacto desde el formulario de KEMPES SOCCER';
+    $mail->Body = "<h3>Contacto desde formulario<h3/>"
+                . "<div style='display:block'>" . "<strong>Nombre: </strong> " . $nombre . "</div><br>"
+                . "<div style='display:block'>" . "<strong>Email: </strong> " . $email . "</div><br>"
+                . "<div style='display:block'>" . "<strong>Pais: </strong> " . $pais . "</div><br>"
+                . "<div style='display:block'>" . "<strong>" . $nombre . " escribio el siguiente mensaje:</strong></div><br> " . $mensaje;
 
-// ---------------------
-// INFORMACION DEL EMAIL
-// ---------------------
-$sender = 'hola@kempessoccer.info';
-$EmailTo = "hola@kempessoccer.info";
-$Subject = "Nuevo mensaje de consulta de la web";
-
-// Cuerpo del email
-$Body = "";
-$Body .= "<strong>Nombre: </strong> " . $nombre2 . "<br>";
-$Body .=  "<strong>Email: </strong> " . $email2 . "<br>";
-$Body .= "<strong>Pais: </strong> " . $pais2 . "<br>";
-$Body .= "<strong>" . $nombre2 . " escribió el siguiente mensaje:</strong><br> " . $mensaje2;
-
-// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
-$headers = 'From:' . $sender. "\n";
-// $headers = "X-Mailer: PHP5\n";
-// $headers .= 'MIME-Version: 1.0' . "\n";
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-
-
-
-
-if ($nombre1 === false || $pais1 === false || $email1 === false || $mensaje1 === false) {
-    header("Location: http://www.kempessoccer.info/?error=true");
-} else {
-    // Si se paso la MEDIDA DE SEGURIDAD 1 enviar email
-    $success = mail($EmailTo, $Subject, $Body, $headers);
-    $success2 = mail('buffajuanalfredo@gmail.com', $Subject, $Body, $headers);
-    // $response = array();
-
-    if ($success && $errorMSG == "") {
-        header("Location: http://www.kempessoccer.info/?error=false");
-        // $response['status'] = 'success';
-        // $response['message'] = 'Tu mensaje fue enviado con exito!!';
+    $mail->AltBody = $nombre . " " . $email . " " . $pais . " " . $mensaje; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+    $status = $mail->send();
+    if(!$status) {
+        echo'<script>window.location.href="http://www.kempessoccer.info/?error=true"; </script>';
+        exit(); 
     } else {
-        header("Location: http://www.kempessoccer.info/?error=true");
+        echo'<script>window.location.href="http://www.kempessoccer.info/?error=false"; </script>';
+        exit();
     }
-}
-
-// 1 - Completa todos los campos
-// 2 - el mensaje contiene headers
 ?>
